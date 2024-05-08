381d5da619056bb23b70dd89efd2c9a5a9428501
pogacsasB <pogacsasb@gmail.com>
2024-05-08 18:00:56 +0200

    commiting

--- infrend_project/models/index.ts
+++ infrend_project/models/index.ts
@@ -1,10 +1,12 @@
 import { Patient } from "../server/src/entity/Patient"
+import { PatientHistory } from "../server/src/entity/PatientHistory"
 
 export interface PatientDTO{
     id: number
     taj: number
     nev: string
     szul_datim: Date
+    History: PatientHistory[]
 }
 
 export interface PatientHistoryDTO{
