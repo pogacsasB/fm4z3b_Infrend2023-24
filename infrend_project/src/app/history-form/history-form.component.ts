bda16ec30d646444024df66a588bee073b3dbaf5
pogacsasB <pogacsasb@gmail.com>
2024-05-08 21:18:15 +0200

    commiting

---
+++ infrend_project/src/app/history-form/history-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-history-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './history-form.component.html',
+  styleUrl: './history-form.component.css'
+})
+export class HistoryFormComponent {
+
+}
