ebf55b72ffefb4499669b2f17ae5c9fefee02b6d
pogacsasB <pogacsasb@gmail.com>
2024-05-08 19:14:16 +0200

    commiting

--- infrend_project/server/src/index.ts
+++ infrend_project/server/src/index.ts
@@ -9,8 +9,8 @@ async function main() {
         const app = express();
         app.use(express.json());
         app.use('/api', getRouter());
-        app.listen(3000, () => {
-            console.log('Listening on port 3000...')
+        app.listen(4200, () => {
+            console.log('Listening on port 4200...')
         });
     } catch (err) {
 
